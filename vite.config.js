653b6791edc8e629246333fd74445d71238f9f0f
Daniel E <danielestupinan0802@gmail.com>
2025-11-20 19:21:42 -0500

    Descripción de cambios

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/gramaticaproject/', // Reemplaza con el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  }
 })
